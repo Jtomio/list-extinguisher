1eaf64f739118a5db76878967faf924b4ec10796
Jeison Tomio <medriase@gmail.com>
2023-10-19 00:22:19 -0300

    :tada: init: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-center">Página 1</h1>
+    </div>
+  )
+}
